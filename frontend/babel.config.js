da8dd78b81ff94269228259196555c47fc837f50
FelipeBriosso <197770080+FelipeBriossoSilver@users.noreply.github.com>
2025-10-29 21:48:01 -0300

    <fix> rebase

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,9 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ['module:@react-native/babel-preset'],
-      ['nativewind/babel'],
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
